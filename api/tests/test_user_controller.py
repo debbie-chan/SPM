58d6031e0a3342a3bb685ae8f9870ae7d63cc594
debbie-chan <71258354+debbie-chan@users.noreply.github.com>
2021-11-06 17:49:40 +0800

    Update test_user.py
    Co-Authored-By: joslyn-ho <73379548+joslyn-ho@users.noreply.github.com>

--- api/tests/test_user_controller.py
+++ api/tests/test_user_controller.py
@@ -59,84 +59,84 @@ class TestUserController(unittest.TestCase):
         self.assertEqual(response_valid.status_code, 200)
         self.assertEqual(response_invalid.status_code, 404)
 
-    def test_createUser(self):
-        request_admin = {
-            "employeeName": "Sally Loh",
-            "designation": "Admin Manager",
-            "department": "Human Resources",
-            "username": "sallyloh",
-            "password": "password",
-            "role": "admin",
-        }
-        request_trainer = {
-            "department": "Technology",
-            "designation": "Senior Engineer",
-            "employeeName": "Emily Mariko",
-            "password": "password",
-            "role": "trainer",
-            "trainedCourses": {"X100": "G1"},
-            "trainingCourses": {"X1010": "G1"},
-            "username": "emilymariko",
-        }
-        request_learner = {
-            "attemptedCourses": {"X200": "G1"},
-            "completedCourses": {"X100": "G1"},
-            "department": "Technology",
-            "designation": "Junior Engineer",
-            "employeeName": "Emma Jones",
-            "enrolledCourses": {"X1010": "G1"},
-            "password": "password",
-            "quizResults": {},
-            "role": "learner",
-            "username": "emmajones",
-        }
+    # def test_createUser(self):
+    #     request_admin = {
+    #         "employeeName": "Sally Loh",
+    #         "designation": "Admin Manager",
+    #         "department": "Human Resources",
+    #         "username": "sallyloh",
+    #         "password": "password",
+    #         "role": "admin",
+    #     }
+    #     request_trainer = {
+    #         "department": "Technology",
+    #         "designation": "Senior Engineer",
+    #         "employeeName": "Emily Mariko",
+    #         "password": "password",
+    #         "role": "trainer",
+    #         "trainedCourses": {"X100": "G1"},
+    #         "trainingCourses": {"X1010": "G1"},
+    #         "username": "emilymariko",
+    #     }
+    #     request_learner = {
+    #         "attemptedCourses": {"X200": "G1"},
+    #         "completedCourses": {"X100": "G1"},
+    #         "department": "Technology",
+    #         "designation": "Junior Engineer",
+    #         "employeeName": "Emma Jones",
+    #         "enrolledCourses": {"X1010": "G1"},
+    #         "password": "password",
+    #         "quizResults": {},
+    #         "role": "learner",
+    #         "username": "emmajones",
+    #     }
 
-        response_admin = self.app.post("/createUser", json=request_admin)
-        response_trainer = self.app.post("/createUser", json=request_trainer)
-        response_learner = self.app.post("/createUser", json=request_learner)
-        self.assertEqual(response_admin.status_code, 201)
-        self.assertEqual(response_trainer.status_code, 201)
-        self.assertEqual(response_learner.status_code, 201)
+    #     response_admin = self.app.post("/createUser", json=request_admin)
+    #     response_trainer = self.app.post("/createUser", json=request_trainer)
+    #     response_learner = self.app.post("/createUser", json=request_learner)
+    #     self.assertEqual(response_admin.status_code, 201)
+    #     self.assertEqual(response_trainer.status_code, 201)
+    #     self.assertEqual(response_learner.status_code, 201)
 
-        response_admin_json = response_admin.get_json()
-        response_trainer_json = response_trainer.get_json()
-        response_learner_json = response_learner.get_json()
-        expected_admin_json = {
-            "_id": response_admin_json["_id"],
-            "employeeName": "Sally Loh",
-            "designation": "Admin Manager",
-            "department": "Human Resources",
-            "username": "sallyloh",
-            "password": "password",
-            "role": "admin",
-        }
-        expected_trainer_json = {
-            "_id": response_trainer_json["_id"],
-            "department": "Technology",
-            "designation": "Senior Engineer",
-            "employeeName": "Emily Mariko",
-            "password": "password",
-            "role": "trainer",
-            "trainedCourses": {"X100": "G1"},
-            "trainingCourses": {"X1010": "G1"},
-            "username": "emilymariko",
-        }
-        expected_learner_json = {
-            "_id": response_learner_json["_id"],
-            "attemptedCourses": {"X200": "G1"},
-            "completedCourses": {"X100": "G1"},
-            "department": "Technology",
-            "designation": "Junior Engineer",
-            "employeeName": "Emma Jones",
-            "enrolledCourses": {"X1010": "G1"},
-            "password": "password",
-            "quizResults": {},
-            "role": "learner",
-            "username": "emmajones",
-        }
-        self.assertEqual(response_admin_json, expected_admin_json)
-        self.assertEqual(response_trainer_json, expected_trainer_json)
-        self.assertEqual(response_learner_json, expected_learner_json)
+    #     response_admin_json = response_admin.get_json()
+    #     response_trainer_json = response_trainer.get_json()
+    #     response_learner_json = response_learner.get_json()
+    #     expected_admin_json = {
+    #         "_id": response_admin_json["_id"],
+    #         "employeeName": "Sally Loh",
+    #         "designation": "Admin Manager",
+    #         "department": "Human Resources",
+    #         "username": "sallyloh",
+    #         "password": "password",
+    #         "role": "admin",
+    #     }
+    #     expected_trainer_json = {
+    #         "_id": response_trainer_json["_id"],
+    #         "department": "Technology",
+    #         "designation": "Senior Engineer",
+    #         "employeeName": "Emily Mariko",
+    #         "password": "password",
+    #         "role": "trainer",
+    #         "trainedCourses": {"X100": "G1"},
+    #         "trainingCourses": {"X1010": "G1"},
+    #         "username": "emilymariko",
+    #     }
+    #     expected_learner_json = {
+    #         "_id": response_learner_json["_id"],
+    #         "attemptedCourses": {"X200": "G1"},
+    #         "completedCourses": {"X100": "G1"},
+    #         "department": "Technology",
+    #         "designation": "Junior Engineer",
+    #         "employeeName": "Emma Jones",
+    #         "enrolledCourses": {"X1010": "G1"},
+    #         "password": "password",
+    #         "quizResults": {},
+    #         "role": "learner",
+    #         "username": "emmajones",
+    #     }
+    #     self.assertEqual(response_admin_json, expected_admin_json)
+    #     self.assertEqual(response_trainer_json, expected_trainer_json)
+    #     self.assertEqual(response_learner_json, expected_learner_json)
 
 
 # Led By: Deborah Chan
