278a6d2a8bc3903185b93b62886cd1dc65e0e339
sherilwy <sherileewy@gmail.com>
2021-11-06 15:03:34 +0800

    Update app.py

--- api/src/app.py
+++ api/src/app.py
@@ -7,6 +7,14 @@ from src.controllers.user_controller import (
     AdminController,
     LearnerController,
 )
+from src.controllers.course_controller import (
+    CourseController,
+    ClassController,
+    LessonController,
+)
+from src.controllers.quiz import Quiz
+from src.controllers.quiz_controller import QuizController
+from src.controllers.question_controller import QuestionController
 from src.database import mongo
 
 
@@ -57,6 +65,12 @@ def create_app(db_uri: str) -> Flask:
         view_func=AdminController.assignLearnerToClass,
     )
     app.add_url_rule(
+        "/verifyLearner/<string:username>/<string:courseCode>"
+        "/<string:classCode>",
+        methods=["GET"],
+        view_func=AdminController.verifyLearner,
+    )
+    app.add_url_rule(
         "/assignTrainerToClass/<string:username>/<string:courseCode>"
         "/<string:classCode>",
         methods=["GET"],
@@ -75,6 +89,71 @@ def create_app(db_uri: str) -> Flask:
         "/<string:classCode>",
         methods=["GET"],
         view_func=LearnerController.deletePendingCourse,
+    )
+
+    # question/quiz routes
+    app.add_url_rule(
+        "/addQuestions",
+        methods=["POST"],
+        view_func=QuestionController.addQuestions,
+    )
+    app.add_url_rule(
+        "/getQuiz/<string:lessonCode>", methods=["GET"], view_func=Quiz.getQuiz
+    )
+    app.add_url_rule(
+        "/gradeQuiz", methods=["POST"], view_func=QuizController.gradeQuiz
+    )
+    app.add_url_rule(
+        "/getFinalGrade/<string:username>/<string:courseCode>",
+        methods=["GET"],
+        view_func=Quiz.getFinalGrade,
+    )
+
+    # course routes
+    app.add_url_rule(
+        "/course", methods=["GET"], view_func=CourseController.getAllCourses
+    )
+    app.add_url_rule(
+        "/course/<string:courseCode>",
+        methods=["GET"],
+        view_func=CourseController.getOneCourse,
+    )
+    app.add_url_rule(
+        "/course/<string:courseCode>/classes",
+        methods=["GET"],
+        view_func=CourseController.getAllClassesInCourse,
+    )
+    app.add_url_rule(
+        "/course/createCourse",
+        methods=["POST"],
+        view_func=CourseController.createCourse,
+    )
+
+    # class routes
+    app.add_url_rule(
+        "/class", methods=["GET"], view_func=ClassController.getAllClasses
+    )
+    app.add_url_rule(
+        "/class/<string:classCode>",
+        methods=["GET"],
+        view_func=ClassController.getOneClass,
+    )
+    app.add_url_rule(
+        "/class/createClass",
+        methods=["POST"],
+        view_func=ClassController.createClass,
+    )
+
+    # lesson routes
+    app.add_url_rule(
+        "/lessons/<string:courseCode>/<string:classCode>",
+        methods=["GET"],
+        view_func=LessonController.getAllLessonsInClass,
+    )
+    app.add_url_rule(
+        "/lesson/createLesson",
+        methods=["POST"],
+        view_func=LessonController.createLesson,
     )
 
     return app
